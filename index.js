0677ce97a27d4b4c0f2846afd23f7b6b7cb90364
KushnirUO <62049925+KushnirUO@users.noreply.github.com>
2022-05-09 20:01:48 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -9,26 +9,64 @@
   const itemWidth = container.clientWidth;
   const movePosition = itemWidth;
 
+
   btnPrev.addEventListener('click',() => {
-    const itemsLeft = Math.abs(position) / itemWidth;
-    if(itemsLeft >= slideToScroll)position += movePosition;
+    findPrevPosition();
     setPosition();
-    checkBtns();
+    makeTimer();
   });
 
   btnNext.addEventListener('click',() => {
-    const itemsLeft = itemsCount - (Math.abs(position) + slideToScroll * itemWidth) / itemWidth;
-    if(itemsLeft >= slideToScroll)position -= movePosition;
+    findNextPosition();
     setPosition();
-    checkBtns();
+    makeTimer();
   });
+
+  const findNextPosition = () => {
+    if(position === -(itemsCount-slideToScroll)*itemWidth)position=555;
+    const itemsRight = itemsCount - (Math.abs(position) / itemWidth) - slideToScroll;
+    if(itemsRight >= slideToScroll)position -= movePosition;
+  };
+
+  const findPrevPosition = () => {
+    if(position === 0)position=-(itemsCount*itemWidth);
+    const itemsLeft = Math.abs(position) / itemWidth;
+    if(itemsLeft >= slideToScroll)position += movePosition;
+  };
+
    const setPosition = () => {
      track.style.transform = `translateX(${position}px)`;
    };
-   const checkBtns = () => {
-     if(position === 0)btnPrev.disabled = 1;
-     else btnPrev.disabled = 0;
-     if(position <= -(itemsCount - slideToScroll) * itemWidth)btnNext.disabled=1;
-     else btnNext.disabled=0;
-   };
-   checkBtns();
+
+  var timer = 0;
+  makeTimer();
+  function makeTimer(){
+    clearInterval(timer)
+    timer = setInterval(function(){
+      findNextPosition();
+      setPosition();
+    },5000);
+  }
+
+
+
+
+
+  const buttonslide1 = document.querySelector('.btn_sld1');
+  const buttonslide2 = document.querySelector('.btn_sld2');
+  const buttonslide3 = document.querySelector('.btn_sld3');
+  const buttonslide4 = document.querySelector('.btn_sld4');
+
+  const changeSlide = (val) => {
+    position=-(val*itemWidth);
+    setPosition();
+    makeTimer();
+  }
+  buttonslide1.addEventListener('click',() => {changeSlide(buttonslide1.value);});
+  buttonslide2.addEventListener('click',() => {changeSlide(buttonslide2.value);});
+  buttonslide3.addEventListener('click',() => {changeSlide(buttonslide3.value);});
+  buttonslide4.addEventListener('click',() => {changeSlide(buttonslide4.value);});
+
+
+
+
